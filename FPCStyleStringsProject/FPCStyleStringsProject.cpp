094026b8542425fb31d888e0c241bc819b03dbc1
maxefimov <efimov.academy.top@gmail.com>
2023-03-15 15:54:34 +0300

    15-03-23

--- FPCStyleStringsProject/FPCStyleStringsProject.cpp
+++ FPCStyleStringsProject/FPCStyleStringsProject.cpp
@@ -10,41 +10,62 @@ void CStrSwap(char*& lstr, char*& rstr);
 
 void CStyleExample()
 {
-    int arr1[]{ 21, 12, 53, 84 };
+    //int arr1[]{ 21, 12, 53, 84 };
 
-    cout << sizeof(arr1) << "\n";
+    //cout << sizeof(arr1) << "\n";
 
-    char ch = 'a';
-    cout << sizeof(ch) << "\n";
+    //char ch = 'a';
+    //cout << sizeof(ch) << "\n";
 
-    char str1[] = { 'H', 'e', 'l', 'l', 'o', '\0' };
-    char str2[] = "Hello";
-    cout << sizeof(str1) << " " << str1 << "\n";
-    cout << sizeof(str2) << " " << str2 << "\n";
+    //char str1[] = { 'H', 'e', 'l', 'l', 'o', '\0' };
+    //char str2[] = "Hello";
+    //cout << sizeof(str1) << " " << str1 << "\n";
+    //cout << sizeof(str2) << " " << str2 << "\n";
 
-    char* str3 = new char[strlen("Hello world") + 1];
-    strcpy_s(str3, strlen("Hello world") + 1, "Hello world");
-    //strcpy(str3, "Hello world");
-    cout << strlen(str3) << " " << str3 << "\n";
+    //char* str3 = new char[strlen("Hello world") + 1];
+    //strcpy_s(str3, strlen("Hello world") + 1, "Hello world");
+    ////strcpy(str3, "Hello world");
+    //cout << strlen(str3) << " " << str3 << "\n";
 
-    //strcpy(str3, "Hello world, hello people");
-    strncpy_s(str3, 13, "Hello world, hello people", 12);
-    cout << str3 << "\n"; // "Hello world,"
+    ////strcpy(str3, "Hello world, hello people");
+    //strncpy_s(str3, 13, "Hello world, hello people", 12);
+    //cout << str3 << "\n"; // "Hello world,"
 
-    char* str4 = new char;
-    strncpy_s(str4, 6, str3 + 6, 5);
-    cout << str4 << "\n";
+    //char* str4 = new char;
+    //strncpy_s(str4, 6, str3 + 6, 5);
+    //cout << str4 << "\n";
 
-    char* n1 = new char[4] { "Bob" };
-    char* n2 = new char[4] { "Joe" };
-    cout << n1 << " " << n2 << "\n";
-    CStrSwap(n1, n2);
-    cout << n1 << " " << n2 << "\n";
+    //char* n1 = new char[4] { "Bob" };
+    //char* n2 = new char[4] { "Joe" };
+    //cout << n1 << " " << n2 << "\n";
+    //CStrSwap(n1, n2);
+    //cout << n1 << " " << n2 << "\n";
+
+    //char* s1 = new char;
+    //strcpy(s1, "Hello");
+
+    //char* s2 = new char;
+    //strcpy(s2, " world");
+
+    //strcat(s1, s2);
+
+    //cout << s1 << "\n"; // "Hello world"
+    //char sym = 'o';
+    //char* fchar = strchr(s1, sym);
+    //if (fchar)
+    //    cout << fchar << "\n";
+
+    //char word[] = "ll";
+    //fchar = strstr(s1, word);
+    //if (fchar)
+    //    cout << fchar << "\n";
+
+    //char* s2 = new char;
 }
 
 void CStyleStringSort()
 {
-    int userCount{ 5 };
+    /*int userCount{ 5 };
     char** users = new char* [userCount];
     for (int i = 0; i < userCount; i++)
         users[i] = new char;
@@ -68,43 +89,105 @@ void CStyleStringSort()
     cout << "\n";
 
     for (int i = 0; i < userCount; i++)
-        cout << users[i] << "\n";
+        cout << users[i] << "\n";*/
 }
 
-void CStrSubstring(char* dest, char* src, int position, int size);
-void CStrSubstring(char* dest, const char* src, int position, int size);
-void CStrReplace(char* dest, char* src, char* oldStr, char* newStr);
-void CStrReplace(char* dest, char* src, const char* oldStr, const char* newStr);
+void CStrSubstring(char*& dest, const char* src, int position, int size);
+
+void CStrReplace(char*& dest, char* src, const char* oldStr, const char* newStr);
 
 int main()
 {
     // CStyleExample();
     // CStyleStringSort();
 
-    char* s1 = new char;
-    strcpy(s1, "Hello");
+    char* str;
+    char cstr[] = "How to Learn the C++ Programming Language";
+    char cstr2[] = "I love lang C++, and lang C++ is good, but C++ and C# actuality";
     
-    char* s2 = new char;
-    strcpy(s2, " world");
+    /*
+    char* begin = strstr(cstr, "the");
+    char* end = strstr(cstr, "Lan");
+    
+    int indexStart = begin - cstr;
+    int size = end - begin - 1;
 
-    strcat(s1, s2);
+    CStrSubstring(str, cstr, indexStart, size); // s2 = lo wor
+    cout << str << "\n";
 
-    cout << s1 << "\n"; // "Hello world"
-    char sym = 'o';
-    char* fchar = strchr(s1, sym);
-    if (fchar)
-        cout << fchar << "\n";
+    char* str2;
 
-    char word[] = "ll";
-    fchar = strstr(s1, word);
-    if (fchar)
-        cout << fchar << "\n";
+    CStrSubstring(str2, str, 5, 5);
 
-    char* s2 = new char;
+    cout << str2 << "\n";
+    */
 
-    CStrReplace(s2, s1, "lo", "fr"); // s2 = "Helfr world";
-    CStrSubstring(s2, s1, 3, 6); // s2 = lo wor
+    CStrReplace(str, cstr2, "C++", ""); // s = "Helfr world";
+    cout << str;
 }
+
+
+
+void CStrSubstring(char*& dest, const char* src, int position, int size)
+{
+    //if (dest) delete[] dest;
+    if (position + size > strlen(src))
+        size -= position;
+
+    dest = new char[size + 1];
+
+    for (int i = 0; i < size; i++)
+        dest[i] = src[i + position];
+    dest[size] = '\0';
+}
+
+void CStrReplace(char*& dest, char* src, const char* oldStr, const char* newStr)
+{
+    int position{};
+    int count{};
+    char* find = src;
+    while (true)
+    {
+        find = strstr(find, oldStr);
+        if (find == nullptr)
+            break;
+        count++;
+        find += strlen(oldStr);
+    }
+
+    int destSize = strlen(src) - count * (strlen(oldStr) - strlen(newStr));
+    dest = new char[destSize + 1];
+
+    int indexSrc{};
+    int indexDest{};
+
+    find = src;
+
+    while (true)
+    {
+        find = strstr(find, oldStr);
+        if (find == nullptr)
+            break;
+        
+        int subSize = find - src - indexSrc;
+
+        for (int i = 0; i < subSize; i++)
+            dest[indexDest++] = src[indexSrc++];
+
+        for (int i = 0; i < strlen(newStr); i++)
+            dest[indexDest++] = newStr[i];
+
+        find += strlen(oldStr);
+        indexSrc += strlen(oldStr);
+        //indexDest += strlen(newStr);
+    }
+    int endSize = strlen(src) - indexSrc;
+    for(int i = 0; i < endSize; i++)
+        dest[indexDest++] = src[indexSrc++];
+    dest[indexDest] = '\0';
+}
+
+
 
 bool CStrCmp(char* lstr, char* rstr)
 {
@@ -117,18 +200,4 @@ void CStrSwap(char*& lstr, char*& rstr)
     strcpy_s(temp, strlen(lstr) + 1, lstr);
     strcpy_s(lstr, strlen(rstr) + 1, rstr);
     strcpy_s(rstr, strlen(temp) + 1, temp);
-}
-
-void CStrSubstring(char* dest, char* src, int position, int size)
-{
-
-}
-void CStrReplace(char* dest, char* src, char* oldStr, char* newStr)
-{
-
-}
-
-void CStrReplace(char* dest, char* src, const char* oldStr, const char* newStr)
-{
-
 }
